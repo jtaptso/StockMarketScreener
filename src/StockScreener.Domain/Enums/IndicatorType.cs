0517d439f218352fbbf18f6ee7b031541ebbc693
Jules Taptso <jules.taptso@psgdover.com>
2026-05-10 08:08:17 +0200

    adding Enumerations

---
+++ src/StockScreener.Domain/Enums/IndicatorType.cs
@@ -0,0 +1,11 @@
+enum IndicatorType
+{
+    RSI, // Relative Strength Index
+    SMA, // Simple Moving Average
+    EMA, // Exponential Moving Average
+    MACD, // Moving Average Convergence Divergence
+    EMACD, // Exponential Moving Average Convergence Divergence
+    BollingerBands, // Bollinger Bands
+    ATR, // Average True Range
+    Unknown
+}
---
+++ src/StockScreener.Domain/Enums/MarketCapCategory.cs
@@ -0,0 +1,9 @@
+public enum MarketCapCategory
+{
+    Micro,
+    Small,
+    Mid,
+    Large,
+    Mega,
+    Unknown
+}
